744cbc59a7baf63c6b9c677e0108b8687d1bfe74
upsehaj <upsehaj@gmail.com>
2018-04-22 22:20:56 +0530

    saturated degree implemented

--- Sudoku_GC_Greedy.cpp
+++ Sudoku_GC_Greedy.cpp
@@ -5,7 +5,24 @@ struct vertex
 {
     int x;
     int y;
+    int sat_deg;
+
+    vertex()
+    {
+        this->x = -1;
+        this->y = -1;
+        this->sat_deg = -1;
+    }
+
+    vertex(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+        this->sat_deg = -1;
+    }
 };
+
+bool comp_degree(struct vertex a, struct vertex b) { return a.sat_deg > b.sat_deg; }
 
 void display(int matrix[9][9])
 {
@@ -36,56 +53,66 @@ int main(int argc, char const *argv[])
     display(s);
     cout << endl;
 
-    // adjacency matrix
-    bool adj[9][9][9][9];
-
+    vector<vertex> adj[9][9];
     for(int i = 0; i < 9; i++)
+    {
         for(int j = 0; j < 9; j++)
+        {
             for(int k = 0; k < 9; k++)
-                for(int l = 0; l < 9; l++)
-                    adj[i][j][k][l] = false;
-
-    for(int i = 0; i < 9; i++)
-        for(int j = 0; j < 9; j++)
-            for(int k = 0; k < 9; k++)
+            {
                 for(int l = 0; l < 9; l++)
                 {
                     if(i == k && j != l)
                     {
-                        adj[i][j][k][l] = true;
-                        adj[k][l][i][j] = true;
+                        vertex temp1(k, l);
+                        adj[i][j].push_back(temp1);
                     }
                     
                     if(j == l && i != k)
                     {
-                        adj[i][j][k][l] = true;
-                        adj[k][l][i][j] = true;
-                    }
-                    
-                    for(int row = 0; row < 3; row++)
-                    {
-                        for(int col = 0; col < 3; col++)
-                        {
-                            if(i != row + i - i%3 || j != col + j - j%3)
-                            {
-                                adj[i][j][row + i - i%3][col + j - j%3] = true;
-                                adj[row + i - i%3][col + j - j%3][i][j] = true;
-                            }
-                        }
-                    }                  
+                        vertex temp1(k, l);
+                        adj[i][j].push_back(temp1);
+                    }                
                 }
+            }
+            for(int row = 0; row < 3; row++)
+            {
+                for(int col = 0; col < 3; col++)
+                {
+                    int p = row + i - i%3;
+                    int q = col + j - j%3;
+                    if(i != p && j != q)
+                    {
+                        vertex temp1(p, q);
+                        adj[i][j].push_back(temp1);
+                    }
+                }
+            } 
+        }
+    }
 
     // list of sudoku boxes
     struct vertex v[81];
     int index = 0;
     for(int i = 0; i < 9; i++)
+    {
         for(int j = 0; j < 9; j++)
         {
+            int count = 0;
+            for(int k = 0; k < 20; k++)
+            {
+                if(s[adj[i][j][k].x][adj[i][j][k].y] != 0)
+                    count++;
+            }
             v[index].x = i;
             v[index].y = j;
+            v[index].sat_deg = count;
             index++;
         }
+    }
     
+    sort(v, v+81, comp_degree);
+
     for(int color = 1; color <= 9; color++)
     {
         for(int i = 0; i < 81; i++)
@@ -95,11 +122,11 @@ int main(int argc, char const *argv[])
             int flag = false;
             if(s[x][y] == 0)
             {
-                for(int j = 0; j < 81; j++)
+                for(int j = 0; j < 20; j++)
                 {
-                    int p = v[j].x;
-                    int q = v[j].y;
-                    if(adj[x][y][p][q] && s[p][q] == color)
+                    int p = adj[x][y][j].x;
+                    int q = adj[x][y][j].y;
+                    if(s[p][q] == color)
                     {
                         flag = true;
                         break;
@@ -113,6 +140,6 @@ int main(int argc, char const *argv[])
 	
 	cout << "Solved Sudoku:" << endl;		
     display(s);  
-	    
+
     return 0;
 }
